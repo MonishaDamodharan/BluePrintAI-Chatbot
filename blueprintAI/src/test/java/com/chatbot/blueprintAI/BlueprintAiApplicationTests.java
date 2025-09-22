fc808bcc7009c0b4048502089b778bbcbc8f7321
Monisha <missmonishavimala@gmail.com>
2025-09-22 16:31:49 +0530

    Base code setup

---
+++ blueprintAI/src/test/java/com/chatbot/blueprintAI/BlueprintAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatbot.blueprintAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueprintAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
